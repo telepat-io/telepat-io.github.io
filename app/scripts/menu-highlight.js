4fbef7a016a94f4c32f4a9e3b3020dfb3b760f38
Gabi Dobocan <gabi@vitalitymedia.ro>
2015-07-14 22:11:38 +0300

    Optimized for mobile devices

--- app/scripts/menu-highlight.js
+++ app/scripts/menu-highlight.js
@@ -1,7 +1,17 @@
 require(['jquery', 'stickUp'], function($, stickUp) {
-  $('.sidebar-nav.second').stickUp({
-    topMargin: 20
-  });
+  var isIpad = window.matchMedia('only screen and (min-device-width : 768px) and (max-device-width : 1024px)').matches;
+  var isIphone = 
+    window.matchMedia('only screen and (min-device-width : 320px) and (max-device-width : 480px)').matches || 
+    window.matchMedia('only screen and (min-device-width : 320px) and (max-device-width : 568px)').matches ||
+    window.matchMedia('only screen and (min-device-width : 375px) and (max-device-width : 667px)').matches ||
+    window.matchMedia('only screen and (min-device-width : 414px) and (max-device-width : 736px)').matches;
+  var isSmallWindow = window.matchMedia('only screen and (max-width : 992px)').matches;
+
+  if (!isIpad && !isIphone && !isSmallWindow) {
+    $('.sidebar-nav.second').stickUp({
+      topMargin: 20
+    });
+  }
 
   var sections = {},
     _height  = $(window).height(),
