86e18a134867a64ba0685b87ea79527a4843640a
Gabi Dobocan <gabi@telepat.io>
2016-08-31 18:59:07 +0300

    Update JS docs

--- scripts/js-sdk.js
+++ scripts/js-sdk.js
@@ -1,15 +1,3 @@
 require(['jquery'], function($) {
-  function updateFile() {
-    var file = window.location.hash.replace('#', '');
-    if (file === '')
-      $('#iframe').attr('src', 'http://docs.telepat.io/telepat-js/README.md.html');
-    else
-      $('#iframe').attr('src', 'http://docs.telepat.io/telepat-js/lib/' + file + '.js.html');
-  }
-
   $('#epsilon-menu').addClass('selected');
-  updateFile();
-  $(window).on('hashchange', function() {
-    updateFile();
-  });
 });
